efb98234764463518c549150940f7b731bbeb5d4
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2024-04-04 13:47:09 -0600

    Removed print statements and corrected expected result

--- tests/test_vector_get_put_exists.py
+++ tests/test_vector_get_put_exists.py
@@ -3,6 +3,7 @@ async def test_vector_put(truncated_vector_client):
         namespace="test",
         key="example",
         record_data={"chocolate": [0 for i in range(1024)]},
+
     )
 
 
@@ -11,7 +12,7 @@ async def test_vector_get(truncated_vector_client):
         namespace="test", key="example", bin_names=["chocolate"]
     )
     assert result.key.namespace == "test"
-    assert result.key.set == "demo"
+    assert result.key.set == ""
     assert result.key.key == "example"
     assert isinstance(result.key.digest, bytes)
     assert result.bins["chocolate"] == [0 for i in range(1024)]
