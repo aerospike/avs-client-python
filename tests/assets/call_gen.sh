30eac7beed9d6f201ca4beaaad6c96ed6bcadf2b
Dominic Pelini <dpelini@aerospike.com>
2024-07-10 10:56:58 -0600

    auth_tls testing

---
+++ tests/assets/call_gen.sh
@@ -0,0 +1,15 @@
+./gen.sh \
+    --tls_maybe y \
+    --rbac_maybe y \
+    --root_certificate_maybe y \
+    --root_certificate_name root \
+    --specify_details_maybe n \
+    --openssl_cnf_maybe n \
+    --password citrusstore \
+    --key_pair_maybe y \
+    --key_password citrusstore \
+    --mutual_auth_maybe y \
+    --client_name brawn \
+    --server_name brawn \
+    --port 5000 \
+    --host brawn
