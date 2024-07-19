4a25454dc2eb5de2499f4efca1d9b32454d411f8
Dominic Pelini <dpelini@aerospike.com>
2024-07-19 09:48:06 -0600

    Update channel_provider.py

--- src/aerospike_vector_search/aio/internal/channel_provider.py
+++ src/aerospike_vector_search/aio/internal/channel_provider.py
@@ -35,10 +35,10 @@ class ChannelProvider(base_channel_provider.BaseChannelProvider):
         service_config_path: Optional[str] = None,
 
     ) -> None:
+        print("\n\n\n BEFORE TEND \n\n\n")
 
         super().__init__(seeds, listener_name, is_loadbalancer, username, password, root_certificate, certificate_chain, private_key, service_config_path)
 
-        print("\n\n\n BEFORE TEND \n\n\n")
         self._tend_initalized: asyncio.Event = asyncio.Event()
         self._tend_ended: asyncio.Event = asyncio.Event()
         self._task: Optional[asyncio.Task] = None
