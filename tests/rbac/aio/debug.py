f4c81dfe4326422cb0a9224f34f09d7bfd14b4b6
Dominic Pelini <dpelini@aerospike.com>
2024-07-21 22:27:35 -0600

    Everything right

--- tests/rbac/aio/debug.py
+++ tests/rbac/aio/debug.py
@@ -1,5 +1,13 @@
 import pytest
+import pytest
+from ...utils import random_int
 
 @pytest.mark.parametrize("test_case", [])
 async def test_add_user(session_rbac_admin_client, test_case):
-    print("YO")
+    await session_rbac_admin_client.add_user(
+        username="admin",
+        password="admin",
+        roles=None
+
+    )
+    assert 1 == 1
