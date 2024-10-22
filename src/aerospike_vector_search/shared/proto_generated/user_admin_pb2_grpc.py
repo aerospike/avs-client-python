2c090d3e9a369276fbba0987f32f9744742e084b
rahulkumar <rkumar@aerospike.com>
2024-10-21 21:45:33 -0700

    Test in CI. Install dependencies using pyproject.toml

--- src/aerospike_vector_search/shared/proto_generated/user_admin_pb2_grpc.py
+++ src/aerospike_vector_search/shared/proto_generated/user_admin_pb2_grpc.py
@@ -7,7 +7,7 @@ from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from . import types_pb2 as types__pb2
 from . import user_admin_pb2 as user__admin__pb2
 
-GRPC_GENERATED_VERSION = '1.66.2'
+GRPC_GENERATED_VERSION = '1.67.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
