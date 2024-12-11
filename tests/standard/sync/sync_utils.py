86939205d57a1ade4831f7ea64829feeed1c18c4
dylan <dwelch@aerospike.com>
2024-12-11 13:50:23 -0800

    ci: add fixtures for index and record setup and teardown

--- tests/standard/sync/sync_utils.py
+++ tests/standard/sync/sync_utils.py
@@ -1,2 +1,12 @@
 def drop_specified_index(admin_client, namespace, name):
     admin_client.index_drop(namespace=namespace, name=name)
+
+def gen_records(count: int, vec_bin: str, vec_dim: int):
+    num = 0
+    while num < count:
+        key_and_rec = (
+            num,
+            { "id": num, vec_bin: [float(num)] * vec_dim}
+        )
+        yield key_and_rec
+        num += 1
