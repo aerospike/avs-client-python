eebb0fc9457269e54353edfdfdce9e1d1a9df8af
dylan <dwelch@aerospike.com>
2025-01-10 11:21:48 -0800

    ci: add unit tests vec-484

--- tests/unit/test_service_config_async.py
+++ tests/unit/test_service_config_async.py
@@ -1,14 +1,15 @@
 import pytest
 from unittest.mock import patch
 
-from aerospike_vector_search import Client, types
+from aerospike_vector_search import types
+from aerospike_vector_search.aio import Client
 
 import grpc
 
-def test_service_config_insecure():
+async def test_service_config():
     # NOTE: this path is relative to the tests directory
     service_config_path = "service_configs/backoff_multiplier.json"
-    with patch("grpc.insecure_channel") as mock_insecure_channel:
+    with patch("grpc.aio.insecure_channel") as mock_insecure_channel:
         with open(service_config_path, "rb") as f:
             service_config_json = f.read()
             try:
@@ -24,14 +25,14 @@ def test_service_config_insecure():
             )
 
 
-def test_service_config_secure():
+async def test_service_config_secure():
     # NOTE: this path is relative to the tests directory
     service_config_path = "service_configs/backoff_multiplier.json"
     # mock ssl_channel_credentials so that it doesn't throw an error
     # when passed the fake root certificate
     with patch("grpc.ssl_channel_credentials") as mock_ssl_channel_credentials:
         mock_ssl_channel_credentials.return_value = "fake_ssl_credentials"
-        with patch("grpc.secure_channel") as mock_secure_channel:
+        with patch("grpc.aio.secure_channel") as mock_secure_channel:
             with open(service_config_path, "rb") as f:
                 service_config_json = f.read()
                 try:
