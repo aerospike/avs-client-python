b9f2c19fd786b7fb189d68f6eadd64648cc652f2
dylan <dwelch@aerospike.com>
2025-01-13 11:44:07 -0800

    ci: add ssl_name_override to channel creation tests

--- tests/unit/test_grpc_channel_creation_async.py
+++ tests/unit/test_grpc_channel_creation_async.py
@@ -6,7 +6,24 @@ from aerospike_vector_search.aio import Client
 
 import grpc
 
-async def test_service_config():
+
+async def test_channel_no_options():
+    # NOTE: this path is relative to the tests directory
+    with patch("grpc.aio.insecure_channel") as mock_insecure_channel:
+        try:
+            client = Client(
+                seeds=types.HostPort(host="localhost", port=8080),
+            )
+        except Exception as e:
+            pass
+
+        mock_insecure_channel.assert_called_with(
+            "localhost:8080",
+            options=None
+        )
+
+
+async def test_channel_insecure():
     # NOTE: this path is relative to the tests directory
     service_config_path = "service_configs/backoff_multiplier.json"
     with patch("grpc.aio.insecure_channel") as mock_insecure_channel:
@@ -16,16 +33,21 @@ async def test_service_config():
                 client = Client(
                     seeds=types.HostPort(host="localhost", port=8080),
                     service_config_path=service_config_path,
+                    ssl_target_name_override="test_ssl_name",
                 )
             except Exception as e:
                 pass
 
             mock_insecure_channel.assert_called_with(
-                "localhost:8080", options=[("grpc.service_config", service_config_json)]
+                "localhost:8080",
+                options=[
+                    ("grpc.ssl_target_name_override", "test_ssl_name"),
+                    ("grpc.service_config", service_config_json)
+                ]
             )
 
 
-async def test_service_config_secure():
+async def test_channel_secure():
     # NOTE: this path is relative to the tests directory
     service_config_path = "service_configs/backoff_multiplier.json"
     # mock ssl_channel_credentials so that it doesn't throw an error
@@ -40,6 +62,7 @@ async def test_service_config_secure():
                         seeds=types.HostPort(host="localhost", port=8080),
                         service_config_path=service_config_path,
                         root_certificate="trigger_secure_path",
+                        ssl_target_name_override="test_ssl_name",
                     )
                 except Exception as e:
                     pass
@@ -47,5 +70,8 @@ async def test_service_config_secure():
                 mock_secure_channel.assert_called_with(
                     "localhost:8080",
                     mock_ssl_channel_credentials.return_value,
-                    options=[("grpc.service_config", service_config_json)]
+                    options=[
+                        ("grpc.ssl_target_name_override", "test_ssl_name"),
+                        ("grpc.service_config", service_config_json)
+                    ]
                 )
