8e140e64ccff72b59433f2f862401f009e6a1db2
dwelch-spike <53876192+dwelch-spike@users.noreply.github.com>
2025-03-20 15:55:32 -0700

    refactor: all client methods use a default timeout of None vec-531 (#84)

--- src/aerospike_vector_search/client.py
+++ src/aerospike_vector_search/client.py
@@ -702,7 +702,7 @@ class Client(BaseClientMixin, AdminBaseClientMixin):
         index_labels: Optional[dict[str, str]] = None,
         index_storage: Optional[types.IndexStorage] = None,
         mode: Optional[types.IndexMode] = None,
-        timeout: Optional[int] = 100_000,
+        timeout: Optional[int] = None,
     ) -> None:
         """
         Create an index.
@@ -796,7 +796,7 @@ class Client(BaseClientMixin, AdminBaseClientMixin):
             index_labels: Optional[dict[str, str]] = None,
             hnsw_update_params: Optional[types.HnswIndexUpdate] = None,
             mode: Optional[types.IndexMode] = None,
-            timeout: Optional[int] = 100_000,
+            timeout: Optional[int] = None,
     ) -> None:
         """
         Update an existing index.
